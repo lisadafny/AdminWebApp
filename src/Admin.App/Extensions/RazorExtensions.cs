894a82a422236b1708db7a55356d46d2cfecb15a
Lisa Dafny <lisadafny.tech@gmail.com>
2023-03-27 22:36:10 -0300

    feature: extension to modify vendor document

---
+++ src/Admin.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Admin.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatDocument(this RazorPage page, int typeOfPerson, string document)
+        {
+            return typeOfPerson == 1 ? Convert.ToUInt64(document).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(document).ToString(@"00\.000\.000/0000\-00");
+        }
+    }
+}
